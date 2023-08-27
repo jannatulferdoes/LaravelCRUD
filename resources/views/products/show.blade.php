207c5ca315be24e18b6576ecf5dff84c7a4139e9
Jannatul Ferdoes <119361193+jannatulferdoes@users.noreply.github.com>
2023-08-27 17:04:46 +0200

    project updated

---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,14 @@
+@extends("layout.app")
+@section("main")
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-sm-8 mt-4">
+            <div class="card p-4">
+                <p>Name : <b>{{$product->name}}</b></p>
+                <p>Description : <b>{{$product->description}}</b></p>
+                <img src="{{asset('products/'.$product->image)}}" class="rounded" width="100%" />
+
+{{--                <img src="products/{{$product->image}}" class="rounded" width="100%" />--}}
+</div>
+@endsection
